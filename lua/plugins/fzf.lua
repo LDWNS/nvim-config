fdecc8910039de4a7b270470179ad51acd1e718c
LDWNS <lukasdownes@gmail.com>
2025-08-15 11:18:33 +0200

    favour fzf over telescope

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,8 +1,20 @@
 return {
-	"ibhagwan/fzf-lua",
-	-- optional for icon support
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- or if using mini.icons/mini.nvim
-	-- dependencies = { "echasnovski/mini.icons" },
-	opts = {},
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    keys = {
+        { '<leader>ff', '<cmd>:lua require("fzf-lua").files()<cr>' },
+        { '<leader>fg', '<cmd>:lua require("fzf-lua").live_grep()<cr>' },
+        { '<leader>fb', '<cmd>:lua require("fzf-lua").buffers()<cr>' },
+        { '<leader>fh', '<cmd>:lua require("fzf-lua").help_tags()<cr>' },
+        { '<leader>fs', '<cmd>:lua require("fzf-lua").git_status()<cr>' },
+        { '<leader>fc', '<cmd>:lua require("fzf-lua").git_commits()<cr>' },
+        { '<leader>fi', '<cmd>:lua require("fzf-lua").git_files()<cr>' },
+    },
+    config = function()
+        require("fzf-lua").setup()
+    end,
+    opts = {}
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,3 @@
--- ~/nvim/lua/slydragonn/plugins/telescope.lua
 
 return {
     "nvim-telescope/telescope.nvim",
@@ -8,15 +7,16 @@ return {
         require("telescope").setup({})
 
         -- set keymaps
-        local keymap = vim.keymap
-        local builtin = require('telescope.builtin')
-        keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-        keymap.set('n', '<leader>fs', builtin.git_status, { desc = 'Telescope git status' })
-        keymap.set('n', '<leader>fc', builtin.git_commits, { desc = 'Telescope git commits' })
-        keymap.set('n', '<leader>fi', builtin.git_files, { desc = 'Telescope git files: ignores .gitignore items' })
+        -- local keymap = vim.keymap
+        -- local builtin = require('telescope.builtin')
+        -- keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        -- keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        -- keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        -- keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        -- keymap.set('n', '<leader>fs', builtin.git_status, { desc = 'Telescope git status' })
+        -- keymap.set('n', '<leader>fc', builtin.git_commits, { desc = 'Telescope git commits' })
+        -- keymap.set('n', '<leader>fi', builtin.git_files, { desc = 'Telescope git files: ignores .gitignore items' })
+        --> replaced by ƒzƒ-lua
     end,
 }
 
