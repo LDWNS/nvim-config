07adb8368d8c8908bdee0605e45bd195ef149d07
LDWNS <lukasdownes@gmail.com>
2025-08-15 11:18:18 +0200

    add trouble commands

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -8,6 +8,16 @@ return {
             "<leader>xx",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
-        }
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+       {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
     }
 }
