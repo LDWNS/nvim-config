54650f9eb030efbb54b7566c36bf7c916b22a245
LDWNS <lukasdownes@gmail.com>
2025-08-15 12:55:14 +0200

    optimize keymaps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,21 +1,22 @@
 return {
     {
         "mfussenegger/nvim-dap",
-        config = function()
-            -- toggles breakpoint
-            vim.keymap.set("n", "<leader>tb", "<cmd>lua require('dap').toggle_breakpoint()<CR>", {})
-        end,
+        keys = {
+            { "<leader>tb", "<cmd>lua require('dap').toggle_breakpoint()<CR>" }
+        }
     },
     { "nvim-neotest/nvim-nio" },
     {
         "rcarriga/nvim-dap-ui",
-        dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        keys = {
+            { "<leader>dt", "<cmd>lua require('dapui').toggle()<CR>" }
+        },
         config = function()
             local dap = require "dap"
             local dapui = require "dapui"
             dapui.setup()
 
-            vim.keymap.set("n", "<leader>dt", "<cmd>lua require('dapui').toggle()<CR>", {})
             dap.listeners.after.event_initialized["dapui_config"] = function()
                 dapui.open()
             end
