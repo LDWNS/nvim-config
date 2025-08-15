c8bf06d79d1d4e952e48759ddcc4bbce0f8a9035
LDWNS <lukasdownes@gmail.com>
2025-08-15 11:09:21 +0200

    formatting

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,47 +20,66 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
---vim.g.autoformat = false
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--vim.g.autoformat = failsafe
 
+
+-- add the following xml files as vim settings
+-- <setting id="org.eclipse.jdt.core.formatter.tabulation.char" value="space"/>
+-- <setting id="org.eclipse.jdt.core.formatter.wrap_before_assignment_operator" value="false"/>
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+--maybe not needed
+--vim.opt.formatoptions = "t"
+
+-- line numbers
 vim.opt.nu = true
 vim.opt.rnu = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 
+-- backups
 vim.opt.swapfile= false
 vim.opt.backup = false
 
+-- search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- colours
 vim.opt.termguicolors = true
+vim.opt.colorcolumn = "120"
 
+--
 vim.opt.scrolloff = 12
 vim.opt.signcolumn = "yes"
+
+-- noice ~ works with 'gf' command (goto file)
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 4200
-
+-- nice border around windows
 vim.opt.winborder = "rounded"
--- vim.opt.winpadding = 2
 
+-- go to next diagnostic
 vim.keymap.set("n", "]d", function()
     vim.diagnostic.jump({count = 1, float = true})
 end
 )
+
+--go to previous diagnostic
 vim.keymap.set("n", "[d", function()
     vim.diagnostic.jump({ count = -1, float = true })
 end
 )
+
+--escape term mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
-vim.opt.colorcolumn = "120"
 
 -- set colorscheme depending on OS
 local os = vim.uv.os_uname().sysname
