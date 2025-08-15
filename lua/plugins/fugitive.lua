557f23a58dfe3d049b22d636070607ff8ad2515c
LDWNS <lukasdownes@gmail.com>
2025-08-15 11:14:41 +0200

    types

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,8 @@
 return {
     "tpope/vim-fugitive",
+    ---@module "fugitive"
+    ---@type fugitive.setupOpts
+    opt ={},
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiff)
