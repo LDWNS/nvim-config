13a9285e75b2b70ac6697acec2f9ecbfa9087ef3
LDWNS <lukasdownes@gmail.com>
2025-09-05 09:23:58 +0900

    clean up lazydev

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,47 +1,12 @@
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua", -- only load on lua files
-		opts = {
-			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                "nvim-dap-ui",
-			},
-		},
-	},
-	{ -- optional cmp completion source for require statements and module annotations
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-		end,
-	},
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-buffer", -- source for text in buffer
-            "hrsh7th/cmp-path", -- source for file system paths
-            {
-                "L3MON4D3/LuaSnip",
-                version = "v2.*",
-                -- install jsregexp (optional!).
-                build = "make install_jsregexp",
-            },
-            "rafamadriz/friendly-snippets",
-            "onsails/lspkind.nvim", -- vs-code like pictograms
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+        library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            "nvim-dap-ui",
         },
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-		end,
     },
 }
