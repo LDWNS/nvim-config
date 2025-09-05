1de77ee6c0157aba01536389145182d7da015960
LDWNS <lukasdownes@gmail.com>
2025-09-05 09:19:27 +0900

    better keymaps

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,8 +1,16 @@
 return {
     {
-      "supermaven-inc/supermaven-nvim",
-      config = function()
-        require("supermaven-nvim").setup({})
-      end,
+        "supermaven-inc/supermaven-nvim",
+        ft = { "java", "lua", "js", "ts" },
+        config = function()
+            require("supermaven-nvim").setup({
+                keymaps = {
+                    accept_suggestion = "<C-S-L>",
+                    clear_suggestion = "<C-S-H>",
+                    accept_word = "<C-S-K>",
+                },
+                ignore_filetypes = { "markdown" },
+            })
+        end,
     },
 }
