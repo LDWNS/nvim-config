ec0f20c9acf94b15706dd6a172a2e0d454a6764a
LDWNS <lukasdownes@gmail.com>
2025-09-05 09:20:07 +0900

    clean up cmp

--- lua/plugins/cmp-handler.lua
+++ lua/plugins/cmp-handler.lua
@@ -1,51 +1,58 @@
--- ~/nvim/lua/slydragonn/plugins/cmp.lua
-
---return {}
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
-		},
-		"rafamadriz/friendly-snippets",
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-	},
-    -- todo: merge lazydev cmp config and this cmp config
-    ---@module "cmp"
-    ---@type cmp.Config
-	config = function()
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
-		local luasnip = require("luasnip")
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-path",   -- source for file system paths
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+    -- require("luasnip.loaders.from_vscode").lazy_load()
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "path" },
-			}),
-		})
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "luasnip",  priority = 100 },
+        { name = "lazydev",  priority = 51, group_index = 1 },
+        { name = "nvim_lsp", priority = 50 },
+        { name = "buffer",   priority = 10 },
+        { name = "path",     priority = 1 },
+      }),
+    })
 
-		vim.cmd([[
+    vim.cmd([[
       set completeopt=menuone,noinsert,noselect
       highlight! default link cmpitemkind cmpitemmenudefault
       ]])
-	end,
+  end,
 }
