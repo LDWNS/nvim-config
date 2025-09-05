050f7ef8da62635c2559525b693b39c782fae2f3
LDWNS <lukasdownes@gmail.com>
2025-09-05 09:25:01 +0900

    clean up lspconf

--- lua/plugins/lspconf.lua
+++ lua/plugins/lspconf.lua
@@ -1,111 +1,102 @@
 -- ~/nvim/lua/slydragonn/plugins/lspconfig.lua
 
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		local nvim_lsp = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local mason_lspconfig = require("mason-lspconfig")
+    local nvim_lsp = require("lspconfig")
 
-		local protocol = require("vim.lsp.protocol")
+    local on_attach = function(client, bufnr)
+      -- Show line diagnostics automatically in hover window
+      vim.o.updatetime = 250
+      vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+    end
 
-		local on_attach = function(client, bufnr)
-			-- format on save
-			if client.server_capabilities.documentFormattingProvider then
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = vim.api.nvim_create_augroup("Format", { clear = true }),
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format()
-					end,
-				})
-			end
-			vim.diagnostic.config({
-				virtual_text = false,
-			})
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    mason_lspconfig.setup({
+      automatic_enable = {
+        exclude = {
+          "jdtls"
+        }
+      },
+      handlers = {
+        ["nvim_lsp"] = function(server)
+          nvim_lsp[server].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ["lua_ls"] = function()
+          nvim_lsp["lua_ls"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["bashls"] = function()
+          nvim_lsp["bashls"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["cssls"] = function()
+          nvim_lsp["cssls"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["tailwindcss"] = function()
+          nvim_lsp["tailwindcss"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["html"] = function()
+          nvim_lsp["html"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["ts_ls"] = function()
+          nvim_lsp["ts_ls"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["jsonls"] = function()
+          nvim_lsp["jsonls"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["eslint"] = function()
+          nvim_lsp["eslint"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["yaml-language-server"] = function()
+          nvim_lsp["yamlls"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["marksman"] = function()
+          nvim_lsp["marksman"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
 
-			-- Show line diagnostics automatically in hover window
-			vim.o.updatetime = 250
-			vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
-		end
-
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		mason_lspconfig.setup({
-			function(server)
-				nvim_lsp[server].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["lua_ls"] = function()
-				nvim_lsp["lua_ls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["bashls"] = function()
-				nvim_lsp["bashls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["cssls"] = function()
-				nvim_lsp["cssls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["tailwindcss"] = function()
-				nvim_lsp["tailwindcss"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["html"] = function()
-				nvim_lsp["html"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["ts_ls"] = function()
-				nvim_lsp["ts_ls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["jsonls"] = function()
-				nvim_lsp["jsonls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["eslint"] = function()
-				nvim_lsp["eslint"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-			["jdtls"] = function()
-				nvim_lsp["jdtls"].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-            ["yaml-language-server"] = function()
-                nvim_lsp["yamlls"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["marksman"] = function()
-                nvim_lsp["marksman"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-		})
-	end,
+        function(server_name)
+          if server_name == "jdtls" then
+            return
+          end
+          require('lspconfig')[server_name].setup({})
+        end,
+      },
+    })
+  end,
 }
