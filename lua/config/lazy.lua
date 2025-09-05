1740ea8749a40af0841e173f0d0c8db4ef3dce28
LDWNS <lukasdownes@gmail.com>
2025-09-05 09:18:43 +0900

    improve spacing and remove duplicate keys

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,17 +23,13 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --vim.g.autoformat = failsafe
 
-
--- add the following xml files as vim settings
--- <setting id="org.eclipse.jdt.core.formatter.tabulation.char" value="space"/>
--- <setting id="org.eclipse.jdt.core.formatter.wrap_before_assignment_operator" value="false"/>
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
 --maybe not needed
 --vim.opt.formatoptions = "t"
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 -- line numbers
 vim.opt.nu = true
@@ -85,14 +81,12 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 local opts = { silent = true, nowait = true }
 vim.keymap.set("n", "fo", vim.lsp.buf.format, opts)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 vim.keymap.set("n", "<C-i>", vim.lsp.buf.signature_help, opts)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
 -- set colorscheme depending on OS
 local os = vim.uv.os_uname().sysname
