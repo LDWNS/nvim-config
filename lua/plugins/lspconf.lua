68b7e8535016834e6ab3dd7acd1d33d4e0267e1e
LDWNS <lukasdownes@gmail.com>
2025-10-12 13:12:25 +0900

    lspconfig & mason improvements

--- lua/plugins/lspconf.lua
+++ lua/plugins/lspconf.lua
@@ -7,102 +7,20 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
-    local mason_lspconfig = require("mason-lspconfig")
-    local nvim_lsp = require("lspconfig")
-
-    local on_attach = function(client, bufnr)
-      -- Show line diagnostics automatically in hover window
-      vim.o.updatetime = 250
-      vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
-    end
-
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    mason_lspconfig.setup({
-      automatic_enable = {
-        exclude = {
-          "jdtls"
-        }
-      },
-      handlers = {
-        ["lemminx"] = function(server)
-          nvim_lsp[server].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["nvim_lsp"] = function(server)
-          nvim_lsp[server].setup({
-            capabilities = capabilities,
-          })
-        end,
-        ["lua_ls"] = function()
-          nvim_lsp["lua_ls"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["bashls"] = function()
-          nvim_lsp["bashls"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["cssls"] = function()
-          nvim_lsp["cssls"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["tailwindcss"] = function()
-          nvim_lsp["tailwindcss"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["html"] = function()
-          nvim_lsp["html"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["ts_ls"] = function()
-          nvim_lsp["ts_ls"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["jsonls"] = function()
-          nvim_lsp["jsonls"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["eslint"] = function()
-          nvim_lsp["eslint"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["yaml-language-server"] = function()
-          nvim_lsp["yamlls"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["marksman"] = function()
-          nvim_lsp["marksman"].setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-          })
-        end,
+    local servers = {
+      "lemminx", "nvim_lsp", "lua_ls",
+      "bashls", "cssls", "tailwindcss",
+      "tailwindcss", "html", "ts_ls",
+      "jsonls", "eslint", "yamlls",
+      "marksman"
+    };
 
-        function(server_name)
-          if server_name == "jdtls" then
-            return
-          end
-          require('lspconfig')[server_name].setup({})
-        end,
-      },
-    })
+    for _, server in ipairs(servers) do
+      vim.lsp.config[server] = {
+        capabilities = capabilities,
+      }
+      vim.lsp.enable(server)
+    end
   end,
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,41 +1,26 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-        },
-        config = function()
-            require("mason").setup({
-                registries = {
-                    "github:mason-org/mason-registry",
-                }
-            })
-
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "cssls",
-                    "eslint",
-                    "html",
-                    "ts_ls",
-                    "lua_ls",
-                    "bashls",
-                    "jsonls",
-                    "tailwindcss",
-                    "jdtls",
-                    "marksman",
-                    "lemminx",
-                },
-            })
-
-            require("mason-tool-installer").setup({
-                ensure_installed = {
-                    "prettier",
-                    "eslint_d",
-                    "java-debug-adapter",
-                    "java-test",
-                },
-            })
-        end,
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lsps
+          "cssls",
+          "eslint",
+          "html",
+          "ts_ls",
+          "lua_ls",
+          "bashls",
+          "jsonls",
+          "tailwindcss",
+          "jdtls",
+          "marksman",
+          "lemminx",
+        -- tools
+          "prettier",
+          "eslint_d",
+          "java-debug-adapter",
+          "java-test",
+      },
     },
+  },
 }
