d73f8aef5e0d317860f5003353950de0d7ae00ca
LDWNS <lukasdownes@gmail.com>
2025-11-04 15:20:23 +0100

    java config

--- lua/plugins/lspconf.lua
+++ lua/plugins/lspconf.lua
@@ -1,4 +1,70 @@
--- ~/nvim/lua/slydragonn/plugins/lspconfig.lua
+local function enableJDTLS()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  -- Eclipse workspace location
+  local plugin_install_location = "/Users/lukasdownes/.local/share/nvim/mason/packages/"
+  local bundles = {
+    vim.fn.glob(
+      plugin_install_location .. "/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+      true)
+  }
+
+  vim.list_extend(bundles,
+    vim.split(vim.fn.glob(plugin_install_location .. "/java-test/extension/server/*.jar", true), "\n"))
+
+  -- Start or attach to language server
+  vim.lsp.config("jdtls", {
+    capabilities = capabilities,
+    settings = {
+      java = {
+        format = {
+          settings = {
+            url = "/Users/lukasdownes/Documents/eclipse-format.xml",
+            profile = "Quarkus",
+          },
+        },
+        completion = {
+          importOrder = {
+            "java",
+            "jakarta",
+            "javax",
+            "ai.timefold",
+            "org",
+            "com",
+          },
+        },
+        sources = {
+          organizeImports = {
+            starThreshold = 999,
+            staticStarThreshold = 999,
+          },
+        },
+        eclipse = {
+          downloadSources = true,
+        },
+        maven = {
+          downloadSources = true,
+        },
+        configuration = {
+          updateBuildConfiguration = "interactive",
+        },
+        implementationsCodeLens = {
+          enabled = true,
+        },
+        referencesCodeLens = {
+          enabled = true,
+        },
+        references = {
+          includeDecompiledSources = true,
+        },
+        signatureHelp = { enabled = true },
+        contentProvider = { preferred = "fernflower" },
+
+      },
+    },
+  })
+
+  vim.lsp.enable("jdtls")
+end
 
 return {
   "neovim/nvim-lspconfig",
@@ -22,5 +88,7 @@ return {
       }
       vim.lsp.enable(server)
     end
+
+    enableJDTLS()
   end,
 }
