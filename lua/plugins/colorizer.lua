505e8d1433ba53db6543ee1f56823e6216a9d0a4
LDWNS <lukasdownes@gmail.com>
2025-11-18 21:33:46 +0100

    better colouring

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,14 +1,14 @@
 -- gives color to colours, e.g. #4f5334
 return {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = {
-        filetypes = {
-            "css",
-            "lua",
-            "javascript",
-            "conf",
-            html = { mode = "foreground" },
-        },
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = {
+    filetypes = {
+      css = { names = false, RRGGBBAA = true, css_fn = true },
+      "lua",
+      "javascript",
+      "conf",
+      html = { mode = "foreground" },
     },
+  },
 }
