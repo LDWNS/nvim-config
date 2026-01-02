71db8d6ad5b07aa0ee9510fdcfaef46a8183de96
LDWNS <lukasdownes@gmail.com>
2026-01-02 13:45:01 +0100

    feat: scratch files

---
+++ lua/plugins/attempt.lua
@@ -0,0 +1,27 @@
+return {
+  'm-demare/attempt.nvim',
+  keys = function()
+    local attempt = require("attempt")
+
+    return {
+      { '<leader>ac', attempt.rename_buf,    mode = "n", desc = "attempt: rename" },
+      { '<leader>ad', attempt.delete_buf,    mode = "n", desc = "attempt: delete" },
+      { '<leader>ai', attempt.new_input_ext, mode = "n", desc = "attempt: new (input)" },
+      { '<leader>al', attempt.open_select,   mode = "n", desc = "attempt: select" },
+      { '<leader>an', attempt.new_select,    mode = "n", desc = "attempt: new (extension)" },
+      { '<leader>ar', attempt.run,           mode = "n", desc = "attempt: run" },
+    }
+  end,
+  config = function()
+    require('attempt').setup {
+      dir = vim.fn.expand('~') .. "/.local/share/attempt.nvim",
+      initial_content = {
+      },
+      ext_options = { 'java', 'lua', 'js', 'py', 'sh', 'md', '' },
+      format_opts = { [''] = '[None]' },
+      run = {
+        java = { 'w !java %' },
+      }
+    }
+  end,
+}
