811243743c521ede5cfa9c9420b4961e915a8727
LDWNS <lukasdownes@gmail.com>
2026-02-03 16:06:52 +0100

    feat: colortheme + find count

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,22 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
-    priority = 1001,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
+  priority = 1001,
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "tokyonight"
+      },
+      sections = {
+        lualine_a = {
+          {
+            'searchcount',
+            maxcount = 9999,
+            timeout = 500,
+          }
+        }
+      }
+    })
+  end,
 }
-
