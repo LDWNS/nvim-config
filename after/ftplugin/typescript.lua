c27e4ade0b6ac56bf5590ea66be6047ed2781acb
LDWNS <lukasdownes@gmail.com>
2026-02-26 17:50:42 +0100

    feat: ts debugger

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,2 +1,34 @@
+local utils = require "dap.utils"
+local dap = require "dap"
 vim.opt_local.makeprg = "npx tsx %"
 vim.keymap.set("n", "<leader><leader>", ":make<CR>")
+
+dap.adapters = {
+  ["pwa-node"] = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = "js-debug-adapter",
+      args = {
+        "${port}",
+      },
+    },
+  },
+}
+
+dap.configurations["typescript"] = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach to process ID",
+    processId = utils.pick_process,
+    cwd = "${workspaceFolder}",
+  },
+}
