86259cc75c164f7a9e5c4928e5d8876fe443a234
LDWNS <lukasdownes@gmail.com>
2026-02-26 17:49:58 +0100

    QOL

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,5 +1,5 @@
 ---@diagnostic disable: undefined-global
--- todo: add the following xml files as vim settings
+-- TODO: add the following xml files as vim settings
 -- <setting id="org.eclipse.jdt.core.formatter.tabulation.char" value="space"/>
 -- <setting id="org.eclipse.jdt.core.formatter.wrap_before_assignment_operator" value="false"/>
 vim.opt.tabstop = 4
@@ -8,6 +8,9 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 -- don't format comments in Java (doesn't get activated -> jdtls is used as formatter)
 vim.opt.formatoptions = "tjlr"
+
+vim.cmd([[command! -nargs=* UpdateOpenAPISpec !mvn clean install -Dupdate-api -Dquickly]])
+
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local plugin_install_location = vim.fn.expand("~/.local/share/nvim/mason/packages")
@@ -60,7 +63,7 @@ config.settings.java = {
       ["org.eclipse.jdt.core.compiler.problem.unusedLocalVariable"] = "warning",
       ["org.eclipse.jdt.core.compiler.problem.unusedPrivateMember"] = "warning",
       ["org.eclipse.jdt.core.compiler.problem.unusedParameter"] = "warning",
-      ["org.eclipse.jdt.core.compiler.problem.unusedImport"] = "warning",
+      ["org.eclipse.jdt.core.compiler.problem.unusedImport"] = "error",
     }
   },
   test = {
